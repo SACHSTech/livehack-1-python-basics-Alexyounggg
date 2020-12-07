cb02626dbd2428725dfdf990907c56a0f89cd567
Alex Young <alex.young23@ycdsbk12.ca>
2020-12-07 18:39:23 +0000

    Finished problem1.py and problem2.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,23 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	Convert the temperature from celsius to fahrenheit 
+
+Author:	Young.A
+
+Created:	7/12/2020
+-----------------------------------------------------------------------------
+'''
+
+print ("*****Welcome to the Celsius Converter Program*****")
+
+# Allow user to input temperature in Celsius
+celsius = float(input("Enter the temperature in Celsius: "))
+
+# Convert the temperature from Celsius to Fahrenhiet 
+fahrenheit = 9/5*celsius + 32
+
+# Output the temperature in Fahrenheit
+print("The temperature is " +str(fahrenheit)+ " Fahrenheit.")
+
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem2.py
+Purpose:	Find the average amount of chicken to be distributed to the students in Mr Fabroa's class and the remainder of the chicken which will go to Mr Fabroa himself 
 
+Author:	Young.A
+
+Created:	7/12/2020
+-----------------------------------------------------------------------------
+'''
+
+print("****Welcome to the Popeyes Chicken Distrubator Program****")
+
+# Input the amount of chicken and students
+students = int(input("How many students are in Mr. Fabroa's class: ")) 
+chicken = int(input("How many Popeyes chicken are there: "))
+
+# Calculate the average amount of chicken and remainder
+average_chicken = chicken//students
+remainder_chicken = chicken%students
+
+# Output the chicken each student and Mr Fabroa will get
+print("Each student in the class will get " +str(average_chicken)+ " Popeye(s) chicken and Mr Fabroa will get " +str(remainder_chicken)+ " Popeye(s) chicken") 
